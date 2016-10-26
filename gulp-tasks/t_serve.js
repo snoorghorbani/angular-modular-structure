55e965dc3b8f597c9340fecfd9439147c27ba10f
snoorghorbani <snoorghorbani@gmail.com>
2016-10-26 13:21:41 +0330

    v2.9.5

--- gulp-tasks/t_serve.js
+++ gulp-tasks/t_serve.js
@@ -5,33 +5,45 @@ var gulp = require('gulp'),
     _ = require('sutility'),
     bs_angular = require('browser-sync').create('bs_angular');
 
+gulp.task('reload', function () {
+    bs_angular.reload();
+})
+
 gulp.task('serve', ['default'], function () {
-    
-    bs_angular.init(_.update({
-        host: "10.0.0.188",
-        proxy: "localhost:20466",
-        port: 3000,
-        notify: true,
-        ui: {
-            port: 3043
-        }
-    }, global.get_gen_conf("t_serve") || {}));
+
+    //bs_angular.init(_.update({
+    //    host: "10.0.0.188",
+    //    proxy: "localhost:20466",
+    //    port: 3000,
+    //    notify: true,
+    //    ui: {
+    //        port: 3043
+    //    }
+    //}, global.get_gen_conf("t_serve") || {}));
+    bs_angular.init({
+        port: 54231 + 1
+        //server: {
+        //    baseDir: "./",
+        //}
+    });
 
     //#region js
-    gulp.watch(global.getPath('js_common'), ['js_common']);
-    gulp.watch(global.getPath('js_angular_common'), ['js_angular_common']);
-    gulp.watch(global.getPath('js_app'), ['js_app']);
-    gulp.watch(global.getPath('html_copy_to_public'), ['html_copy_to_public']);
-    gulp
-        .watch(['public/assets/js/angular_common.js',
-            'public/assets/js/app.js',
-            'public/assets/js/common.js',
-            'public/client/**/*.js'
-        ])
-        .on('change', bs_angular.reload);
+
+    gulp.watch(global.getPath('js_common'), ['js_common' , 'reload']);
+    gulp.watch(global.getPath('js_angular_common'), ['js_angular_common', 'reload']);
+    gulp.watch(global.getPath('js_app'), ['js_app', 'reload']);
+    gulp.watch(global.getPath('js_copy_to_public'), ['js_copy_to_public', 'reload']);
+    //gulp
+    //    .watch(['public/assets/js/angular_common.js',
+    //            'public/assets/js/app.js',
+    //            'public/assets/js/common.js'
+    //    ])
+    //    .on('change', bs_angular.reload);
+
     //#endregion
 
     //#region JSON
+
     gulp.watch(global.getPath('json_minify'), ['json_minify']);
     gulp
         .watch('public/languages/**/*.json')
@@ -40,18 +52,19 @@ gulp.task('serve', ['default'], function () {
     //#endregion
 
     //#region css
+
     gulp.watch('public/assets/less/**/*.less', ['less_main']);
     gulp
         .watch('public/assets/css/main.css')
         .on('change', function () {
             bs_angular.reload("public/assets/css/main.css")
         });
+
     //#endregion
 
-    gulp.watch('public/client/**/*.html', ['html_copy_to_public']);
-    gulp
-        .watch([
-            'public/*.html'
-        ])
-        .on('change', bs_angular.reload);
+    //#region html
+
+    gulp.watch('client/**/*.html', ['html_copy_to_public', 'reload']);
+
+    //#endregion
 });
